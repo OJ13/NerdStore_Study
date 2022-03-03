22c0f776243b9041a213a72ddaedb8247fa082f6
Osmar Junior <15853139+OJ13@users.noreply.github.com>
2022-03-03 18:35:28 -0300

    add configurações de jwt

--- MainSolution/src/services/NSE.Identidade.API/Startup.cs
+++ MainSolution/src/services/NSE.Identidade.API/Startup.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
@@ -5,9 +8,10 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using NSE.Identidade.API.Data;
-using System;
+using NSE.Identidade.API.Extensions;
 
 namespace NSE.Identidade.API
 {
@@ -22,15 +26,49 @@ namespace NSE.Identidade.API
 
         public void ConfigureServices(IServiceCollection services)
         {
+            var appSettingsSection = Configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSettingsSection);
+
+            var appSettings = appSettingsSection.Get<AppSettings>();
+
+            services.AddControllers();
+
+            #region EntityFramework Config
             services.AddDbContext<ApplicationDbContext>(optionsAction: options =>
                 options.UseSqlServer(Configuration.GetConnectionString(name: "DefaultConnection")));
+            #endregion
 
+            #region Identity Config
             services.AddDefaultIdentity<IdentityUser>()
                 .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
+            #endregion
 
-            services.AddControllers();
+            #region JWT Config
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+
+            services.AddAuthentication(configureOptions: options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(bearerOptios =>
+            {
+                bearerOptios.RequireHttpsMetadata = true;
+                bearerOptios.SaveToken = true;
+                bearerOptios.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidAudience = appSettings.ValidoEm,
+                    ValidIssuer = appSettings.Emissor
+                };
+            });
+            #endregion
+
+            #region Swagger Config
 
             services.AddSwaggerGen(c =>
             {
@@ -46,6 +84,7 @@ namespace NSE.Identidade.API
                     }
                 });
             });
+            #endregion
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
