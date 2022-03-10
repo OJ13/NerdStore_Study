b87a7a71e9bd9e728e7454f348d59265f74e04d7
Osmar Junior <15853139+OJ13@users.noreply.github.com>
2022-03-10 19:00:13 -0300

    web - add view, model e estrutura

---
+++ MainSolution/src/web/NSE.WebApp.MVC/Controllers/IdentidadeController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NSE.WebApp.MVC.Models;
+using System.Threading.Tasks;
+
+namespace NSE.WebApp.MVC.Controllers
+{
+    public class IdentidadeController : Controller
+    {
+        [HttpGet]
+        [Route("nova-conta")]
+        public IActionResult Registro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("nova-conta")]
+        public async Task<IActionResult> Registro(UsuarioRegistro usuarioRegistro)
+        {
+            if (!ModelState.IsValid)
+                return View(usuarioRegistro);
+
+            //Verificação em API
+            if (false) return View(usuarioRegistro);
+
+            return RedirectToAction(actionName: "Index", controllerName: "Home");
+        }
+
+        [HttpGet]
+        [Route("login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login(UsuarioLogin usuarioLogin)
+        {
+            if (!ModelState.IsValid)
+                return View(usuarioLogin);
+
+            //Verificação em API
+            if (false) return View(usuarioLogin);
+
+            return RedirectToAction(actionName: "Index", controllerName: "Home");
+        }
+
+        [HttpGet]
+        [Route("sair")]
+        public async Task<IActionResult> Logout(UsuarioLogin usuarioLogin)
+        {
+            return RedirectToAction(actionName: "Index", controllerName: "Home");
+        }
+    }
+}
--- MainSolution/src/web/NSE.WebApp.MVC/Controllers/IdentidadeController1.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace NSE.WebApp.MVC.Controllers
-{
-    public class IdentidadeController1 : Controller
-    {
-    }
-}
