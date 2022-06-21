10defc9405b20e1eb97a6323a93aff352242979c
Osmar Junior <15853139+OJ13@users.noreply.github.com>
2022-06-20 23:33:11 -0300

    add ctalogo controller em mvc

---
+++ MainSolution/src/web/NSE.WebApp.MVC/Services/ICatalogoService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using NSE.WebApp.MVC.Models;
+using NSE.WebApp.MVC.Extensions;
+using System.Collections.Generic;
+using Microsoft.Extensions.Options;
+
+namespace NSE.WebApp.MVC.Services
+{
+    public interface ICatalogoService
+    {
+        Task<IEnumerable<ProdutoViewModel>> ObterTodos();
+        Task<ProdutoViewModel> ObterPorId(Guid id);
+    }
+
+    public class CatalogoService : Service, ICatalogoService
+    {
+        private readonly HttpClient _httpClient;
+        public CatalogoService(HttpClient httpClient,
+            IOptions<AppSettings> settings)
+        {
+            httpClient.BaseAddress = new Uri(settings.Value.CatalogoUrl);
+            _httpClient = httpClient;
+        }
+        public async Task<IEnumerable<ProdutoViewModel>> ObterTodos()
+        {
+            var response = await _httpClient.GetAsync(requestUri: "/catalogo/produtos");
+
+            TratarErrosResponse(response);
+
+            return await DeserializarObjetoResponse<IEnumerable<ProdutoViewModel>>(response);
+        }
+        public async Task<ProdutoViewModel> ObterPorId(Guid id)
+        {
+            var response = await _httpClient.GetAsync(requestUri: $"/catalogo/produtos/{id}");
+
+            TratarErrosResponse(response);
+
+            return await DeserializarObjetoResponse<ProdutoViewModel>(response);
+        }
+    }
+}
