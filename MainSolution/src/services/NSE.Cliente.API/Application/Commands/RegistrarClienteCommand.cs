b20d2110fc4407d797d38154221a96578f1ef2b9
Osmar Junior <15853139+OJ13@users.noreply.github.com>
2022-08-30 19:19:49 -0300

    add camada de validação ao command

--- MainSolution/src/services/NSE.Cliente.API/Application/Commands/RegistrarClienteCommand.cs
+++ MainSolution/src/services/NSE.Cliente.API/Application/Commands/RegistrarClienteCommand.cs
@@ -1,4 +1,5 @@
-﻿using NSE.Core.Messages;
+﻿using FluentValidation;
+using NSE.Core.Messages;
 using System;
 
 namespace NSE.Clientes.API.Application.Commands
@@ -17,6 +18,45 @@ namespace NSE.Clientes.API.Application.Commands
             Nome = nome;
             Email = email;
             Cpf = cpf;
+        }
+
+        public override bool EhValido()
+        {
+            ValidationResult = new RegistrarClienteValidation().Validate(instance: this);
+            return ValidationResult.IsValid;
+        }
+
+        //CLASSE ANINHADA
+        public class RegistrarClienteValidation : AbstractValidator<RegistrarClienteCommand>
+        {
+            public RegistrarClienteValidation()
+            {
+                RuleFor(expression: c => c.Id)
+                    .NotEqual(toCompare: Guid.Empty)
+                    .WithMessage("Id do Cliente inválido");
+
+                RuleFor(c => c.Nome)
+                    .NotEmpty()
+                    .WithMessage("O nome do cliente não foi informado");
+
+                RuleFor(c => c.Cpf)
+                    .Must(TerCpfValido)
+                    .WithMessage("O CPF informado não é válido");
+
+                RuleFor(c => c.Email)
+                    .Must(TerEmailValido)
+                    .WithMessage("O email informado não é válido");
+            }
+
+            public static bool TerCpfValido(string cpf)
+            {
+                return Core.DomainObjects.Cpf.Validar(cpf);
+            }
+
+            public static bool TerEmailValido(string email)
+            {
+                return Core.DomainObjects.Email.Validar(email);
+            }
         }
     }
 }
