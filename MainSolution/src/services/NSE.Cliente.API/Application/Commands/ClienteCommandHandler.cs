eb96c08bb25482f0f00cdd932d06701a0212166d
Osmar Junior <15853139+OJ13@users.noreply.github.com>
2022-08-29 22:40:29 -0300

    add inicio de Command e config de MediatR

---
+++ MainSolution/src/services/NSE.Cliente.API/Application/Commands/ClienteCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation.Results;
+using MediatR;
+using NSE.Clientes.API.Models;
+using NSE.Core.Messages;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NSE.Clientes.API.Application.Commands
+{
+    public class ClienteCommandHandler : CommandHandler,
+        IRequestHandler<RegistrarClienteCommand, ValidationResult>
+    {
+        public async Task<ValidationResult> Handle(RegistrarClienteCommand message, CancellationToken cancellationToken)
+        {
+            if (!message.EhValido())
+                return message.ValidationResult;
+
+            var cliente = new Cliente(message.Id, message.Nome, message.Email, message.Cpf);
+
+            //Validacoes de negocio
+
+            //Persistir no banco
+            if (true) //ex.: regra de duplicidade
+            {
+                AdicionarErro("Esse Cpf já esta em uso");
+                return ValidationResult;
+            }
+
+            return message.ValidationResult;
+        }
+    }
+}
