b0e66de9c77297d882d43221cc29622a3e0e0892
Osmar Junior <15853139+OJ13@users.noreply.github.com>
2022-09-13 22:35:17 -0300

    novo método de adicionar erro de processamenro

--- MainSolution/src/building blocks/NSE.WebApi.Core/Controllers/MainController.cs
+++ MainSolution/src/building blocks/NSE.WebApi.Core/Controllers/MainController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,6 +27,16 @@ namespace NSE.WebApi.Core.Controllers
         {
             var erros = modelState.Values.SelectMany(e => e.Errors);
             foreach (var erro in erros)
+            {
+                AdicionarErroProcessamento(erro.ErrorMessage);
+            }
+
+            return CustomResponse();
+        }
+
+        protected ActionResult CustomResponse(ValidationResult validationResult)
+        {
+            foreach (var erro in validationResult.Errors)
             {
                 AdicionarErroProcessamento(erro.ErrorMessage);
             }
