43df7b951b1d4d4a1186b1c6121ebbe0a29bbadd
Osmar Junior <15853139+OJ13@users.noreply.github.com>
2022-09-27 22:26:56 -0300

    init evento e message de integracao

---
+++ MainSolution/src/building blocks/NSE.Core/Messages/Integration/IntegraionEvent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace NSE.Core.Messages.Integration
+{
+    public abstract class IntegraionEvent : Event
+    {
+    }
+
+    public class UsuarioRegistradoIntegrationEvent : IntegraionEvent
+    {
+        public UsuarioRegistradoIntegrationEvent(Guid id, string nome, string email, string cpf)
+        {
+            Id = id;
+            Nome = nome;
+            Email = email;
+            Cpf = cpf;
+        }
+        public Guid Id { get; private set; }
+        public string Nome { get; private set; }
+        public string Email { get; private set; }
+        public string Cpf { get; private set; }
+    }
+}
