658a6437685ca74e94992ee5729110ed2714acfb
Osmar Junior <15853139+OJ13@users.noreply.github.com>
2022-10-17 21:36:02 -0300

    criando um servicework para executar o tempo todo em background

--- MainSolution/src/services/NSE.Cliente.API/Configuration/DependencyInjectionConfig.cs
+++ MainSolution/src/services/NSE.Cliente.API/Configuration/DependencyInjectionConfig.cs
@@ -5,6 +5,7 @@ using NSE.Clientes.API.Application.Commands;
 using NSE.Clientes.API.Application.Events;
 using NSE.Clientes.API.Data;
 using NSE.Clientes.API.Data.Repository;
+using NSE.Clientes.API.Services;
 using NSE.Core.Mediator;
 
 namespace NSE.Clientes.API.Configuration
@@ -18,6 +19,7 @@ namespace NSE.Clientes.API.Configuration
             services.AddScoped<ClientesContext>();
             services.AddScoped<IClienteRepository, ClienteRepository>();
             services.AddScoped<INotificationHandler<ClienteRegistradoEvent>, ClienteEventHandler>();
+            services.AddHostedService<RegistroClienteIntegrationHandler>();
         }
     }
 }
